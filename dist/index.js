f45c8ce59437c617190f26eef92410c3a97f3e8d
Fahmi Akbar <fahmialiasakbar@gmail.com>
2021-10-14 20:52:08 +0700

    Types, Enum, Object

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,41 @@
 "use strict";
 let id = 5;
-console.log('ID:', id);
+let company = 'Dinata';
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4];
+let arr = ['Man', 2, false];
+// Tuple
+let person = [1, 'Brad', true];
+// Tupple Array
+let employees;
+employees = [
+    [1, 'Maia'],
+    [2, 'Nikita'],
+    [3, 'Brad']
+];
+// Union
+let pid;
+pid = 22;
+pid = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1);
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2);
+const user = {
+    id: 1,
+    name: 'Dimitrov'
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,56 @@
-let id: number = 5;
-console.log('ID:', id)
+let id: number = 5
+let company: string = 'Dinata'
+let isPublished: boolean = true
+let x: any = 'Hello'
+
+let ids: number[] = [1,2,3,4]
+
+let arr: any[] = ['Man', 2, false]
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true]
+
+// Tupple Array
+let employees: [number, string][]
+
+employees = [
+    [1, 'Maia'],
+    [2, 'Nikita'],
+    [3, 'Brad']
+]
+
+// Union
+let pid: string | number
+pid = 22
+pid = '22'
+
+// Enum
+enum Direction1 {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+console.log(Direction1)
+
+enum Direction2 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right'
+}
+
+console.log(Direction2)
+
+// Object
+
+type User = {
+    id: number,
+    name: string
+} 
+
+const user: User = {
+    id: 1,
+    name: 'Dimitrov'
+}
